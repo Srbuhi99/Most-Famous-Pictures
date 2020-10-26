7481776ac1157330496d28c18f66bfed9b084b01
Srbuhi Danielyan <srbuhidanielyan2018@gmail.com>
2020-10-26 16:36:17 +0400

    Artist, Picture models

---
+++ app/src/main/java/com/example/mostfamouspictures/model/Artist.kt
@@ -0,0 +1,12 @@
+package com.example.mostfamouspictures.model
+
+import com.google.firebase.firestore.PropertyName
+
+data class Artist(
+    @set:PropertyName("artist_id") @get:PropertyName("artist_id") var artistId:String,
+    @set:PropertyName("artist_name") @get:PropertyName("artist_name") var artistName:String,
+    var url:String,
+    var biography:String
+    ){
+    constructor():this("","","","")
+}
