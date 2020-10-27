cd47852da35f065ac3e16561d02db405dd01f8c4
Srbuhi Danielyan <srbuhidanielyan2018@gmail.com>
2020-10-27 11:41:39 +0400

    ArtistListFragment,PicturesListFragment, Added ViewPager and TabLayout

--- app/src/main/java/com/example/mostfamouspictures/view/fragment/MainFragment.kt
+++ app/src/main/java/com/example/mostfamouspictures/view/fragment/MainFragment.kt
@@ -6,14 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mostfamouspictures.R
+import com.example.mostfamouspictures.view.adapter.MainViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +19,43 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViewPager()
+        setupTabLayout()
+    }
+
+    private fun setupViewPager() {
+
+        val adapter = MainViewPagerAdapter(
+            childFragmentManager
+        )
+        main_view_pager.adapter = adapter
+
+    }
+
+    private fun setupTabLayout(){
+        main_tab_layout.tabGravity = TabLayout.GRAVITY_FILL
+
+        main_view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(main_tab_layout))
+
+        main_tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+
+                main_view_pager.currentItem = tab.position
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
     }
 
 
